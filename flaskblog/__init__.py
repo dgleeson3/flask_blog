ebb529899b9e1287861ec2693045fcb8f0c03e75
Denis Gleeson <dgleeson3@gmail.com>
2024-11-21 22:57:26 +0000

    added code to initalise Database for Render deployment

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -4,9 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from sqlalchemy import create_engine
+import sqlalchemy as sa
 
-
-# now interfacing to postgresql db vgr_6db ( 180824 )
+# now interfacing to postgresql db vgr_12db ( 181124 )
 # https://stackoverflow.com/questions/62113733/how-connect-postgresql-database-with-sqlalchemy-python
 # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
 app = Flask(__name__)
@@ -25,3 +26,20 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 # needed this placed here to allow site to run
 from flaskblog import routes
+
+# For hosting in Render I need to initalise the database
+# check if it needs to be initalised first.
+# https://testdriven.io/blog/flask-render-deployment/
+# https://gitlab.com/patkennedy79/flask_user_management_example/-/blob/2be600d0bfd5a28957b4961bcb2a6607a9c07b08/project/__init__.py#L42
+# Check if the database needs to be initialized
+engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sa.inspect(engine)
+if not inspector.has_table("user"):
+    print("Init the db ")
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        app.logger.info('Initialized the database!')
+else:
+    print("  db  already initalised ")
+    app.logger.info('Database already contains the users table.')
