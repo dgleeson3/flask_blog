7b413cd4609de426262da46fae1983cb9258800e
Denis Gleeson <dgleeson3@gmail.com>
2025-06-01 13:02:54 +0100

    change port to 10000

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ if __name__ == '__main__':
   with app.app_context():
     db.create_all()    
     app.run(debug=True)
-    app.run(host='0.0.0.0', port=80, debug=True)
+    # origional one commented out at some stage
+#    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
 
 
